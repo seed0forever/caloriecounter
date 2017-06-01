1c6a2bf14b74fdff3fc83ab2b5719f9930ad6e0c
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-06-01 12:06:35 +0200

    initial commit

---
+++ src/main/java/com/greenfox/seed0forever/caloriecounter/CaloriecounterApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.seed0forever.caloriecounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloriecounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaloriecounterApplication.class, args);
+	}
+}
